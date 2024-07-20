dd49103407154e60896fa3d75709200d48ea774c
Amirmasoud-Rahimi <amirmasoudrahimiofficial@gmail.com>
2024-07-20 08:45:02 +0330

    lets go

---
+++ src/main/java/com/project/api/restful_user_api/RestfulUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.api.restful_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/api/restful_user_api/RestfulUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api.restful_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
